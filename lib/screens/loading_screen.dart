12142ef1627bbeac2333674ac630f22ed3d8075b
Yuta iwasaki <yanyong0127@gmail.com>
2022-03-12 14:28:04 +0900

    http通信について

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
+import 'package:clima_flutter/utilities/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
 import '../services/location.dart';
+import 'package:http/http.dart' as http; //パッケージの何を使用するかasでわかりやすくしてる
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,7 +10,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -18,16 +18,33 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void fetchLocation() async {
     Location location = Location();
-  await location.fetchCurrentLocation();
-  print('fetchLocation colled');
-    print(location.latitude);
+    await location.fetchCurrentLocation();
     print(location.longitude);
   }
 
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35.67567567567568&lon=139.7601531564893&units=metric&lang=ja&appid=$kMYAPI'));
 
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodeData = jsonDecode(data);
+
+      double temperature = decodeData['main']['temp'];
+      int conditionId = decodeData['weather'][0]['id'];
+      String cityname = decodeData['name'];
+      print(temperature);
+      print(conditionId);
+      print(cityname);
+    } else {
+      print(response.statusCode);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    fetchData();
     return Scaffold();
   }
 }
